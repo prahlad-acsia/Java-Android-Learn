28d7e0f4af334780f8815be80a8c9bbdbb2d697d
Prahlad <prahlad.bk@acsiatech.com>
2025-02-04 16:15:31 +0530

    first

--- Sample.java
+++ Sample.java
@@ -1,5 +1,13 @@
 class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int num = 1;       //primitive
+        float fl = 1233L;  //primitive
+        char ch = '1';     //primitive
+        String str = "ppbkjk";
+        System.out.println("Hello world         "+ch);
+        System.out.println("char at-"+str.charAt(2));
+        System.out.println("length "+str.length());
+        System.out.println("sub string "+str.substring(0,2));
+        // System.out.println(str.replace("p", "A"));
     }
 }
